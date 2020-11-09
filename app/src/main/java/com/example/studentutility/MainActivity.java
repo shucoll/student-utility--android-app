df6c29ecd055b469e02118c688d1714d1ff1a43e
Shuvam <ssiwakoti12@gmail.com>
2020-11-09 19:39:51 +0545

    launcher icon added

--- app/src/main/java/com/example/studentutility/MainActivity.java
+++ app/src/main/java/com/example/studentutility/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.studentutility;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
